e17f2f69dfe34f22e2deaa51b8342e71ee796b13
Jack Treble <jack.treble@kaluza.com>
2020-10-01 17:15:56 +0100

    implement SelectionButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import './App.css';
 import Admin from './pages/admin'
+import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 import Home from './pages/home';
+import SelectionButton from "./Components/SelectionButton";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/admin">
-            <Admin />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/admin">
+                        <Admin/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Router>
+
+            <SelectionButton text={{zone: "red"}} color="red" onClick={() => console.log("beans")}/>
+            <SelectionButton text={{zone: "dog"}} color="red" animal="🐶" onClick={() => console.log("beans")}/>
+            <SelectionButton text={{transport: "car"}} transport="🚘" onClick={() => console.log("beans")}/>
+            <SelectionButton text={{zone: "red dog", transport: "car"}} color="red" animal="🐶" transport="🚘"
+                             onClick={() => console.log("beans")}/>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/SelectionButton.tsx
@@ -0,0 +1,69 @@
+import React, {FC} from 'react';
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    root: {
+        minHeight: 345,
+        maxWidth: 345
+    },
+    bigEmoji: {
+        fontSize: 192
+    },
+    smallEmoji: {
+        fontSize: 96
+    }
+});
+
+type SelectionButtonText = {
+    zone?: string
+    transport?: string
+}
+
+type SelectionButtonProps = {
+    color?: string
+    animal?: string
+    transport?: string
+    text: SelectionButtonText
+    onClick : () => void
+}
+
+const SelectionButton: FC<SelectionButtonProps> = (props) => {
+
+    const classes = useStyles();
+
+    let emoji = () => {
+        if (props.animal && props.transport) {
+            return <>
+                <Typography component="div" className={classes.smallEmoji}>
+                    {props.animal}
+                </Typography>
+                <Typography component="div" className={classes.smallEmoji}>
+                    {props.transport}
+                </Typography></>
+        } else {
+            return <Typography component="div" className={classes.bigEmoji}>
+                {props.animal || props.transport || '\u00A0'}
+            </Typography>
+        }
+    }
+    return <Card className={classes.root}>
+        <CardActionArea className={classes.root} onClick={() => props.onClick()}>
+            <CardContent style={{backgroundColor: props.color}}>
+                {emoji()}
+            </CardContent>
+            <Typography variant="h3" gutterBottom component="h3">
+                {props.text.zone}
+            </Typography>
+            <Typography variant="h3" gutterBottom component="h3">
+                {props.text.transport}
+            </Typography>
+        </CardActionArea>
+    </Card>
+}
+
+export default SelectionButton
