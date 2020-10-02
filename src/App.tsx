eda9da87a6222f65812d31e9ec387abfbe4f944d
Gwen Tibbles <gwen.tibbles@ovoenergy.com>
2020-10-02 08:33:04 +0100

    changed font to match Energy Sparks site

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,32 @@ import "./App.css";
 import Admin from "./pages/admin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
-// BEGIN: TO BE REMOVED WHEN REAL DATA IS SAVED TO STORAGE
-function createData(
-    colour: string,
-    animal: string,
-    transport: string,
-    date: Date,
-    am_pm: string
-) {
-    return {
-        zone: { colour, animal },
-        transport,
-        date,
-        am_pm,
-    };
-}
-
-const rows = [
-    createData("red", "tiger", "car", new Date(), "am"),
-    createData("blue", "dog", "bus", new Date(), "am"),
-    createData("red", "tiger", "car", new Date(), "am"),
-];
-
-// localStorage.setItem("data", JSON.stringify(rows));
-// END: TO BE REMOVED WHEN REAL DATA IS SAVED TO STORAGE
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: [
+            'Quicksand',
+            'sans-serif',
+        ].join(','),
+    },
+});
 
 function App() {
     return (
         <div className="App">
-            <Router>
-                <Switch>
-                    <Route path="/admin">
-                        <Admin />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Switch>
+                        <Route path="/admin">
+                            <Admin />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ThemeProvider>
         </div>
     );
 }
