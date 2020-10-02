3af63a7564c0d287507498816a6cb919d53cc280
James Davenport <james.davenport@kaluza.com>
2020-10-02 09:54:29 +0100

    Reload admin page on clearing results

--- src/Components/AdminSubmit.tsx
+++ src/Components/AdminSubmit.tsx
@@ -18,65 +18,93 @@ function doSubmit(payload: Array<transportData>) {
 
 function Buttons({ payload }: { payload: Array<transportData> }) {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        maxWidth: "220px",
+        margin: "auto",
+      }}
+    >
       <Button
         variant="contained"
         color="secondary"
-        onClick={() => localStorage.removeItem(localStorageKey)}
+        onClick={() => {
+          localStorage.removeItem(localStorageKey);
+          window.location.reload(false);
+        }}
       >
         Clear Data
-  </Button>
+      </Button>
       <Button
         variant="contained"
         color="primary"
         onClick={() => doSubmit(payload)}
       >
         Submit
-    </Button>
-    </>
+      </Button>
+    </div>
   );
 }
 
 function AdminSubmit() {
-  const transportRows: transportData[] =
-    JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  const transportRows: transportData[] = JSON.parse(
+    localStorage.getItem(localStorageKey) || "[]"
+  );
 
   return (
     <div>
-      <p>Please confirm and submit the below entries.</p>
-      <Buttons payload={transportRows} />
-      <div style={{ margin: "20px" }}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Colour</TableCell>
-                <TableCell>Animal</TableCell>
-                <TableCell>Transport</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>AM/PM</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {transportRows.map((row: transportData, index: number) => {
-                const { zone, transport, date, am_pm } = row;
-                return (
-                  <TableRow
-                    key={`${row.zone.animal}-${zone.colour}-${transport}-${date}-${am_pm}-${index}`}
-                  >
-                    <TableCell>{zone.colour}</TableCell>
-                    <TableCell>{zone.animal}</TableCell>
-                    <TableCell>{transport}</TableCell>
-                    <TableCell>{date}</TableCell>
-                    <TableCell>{am_pm}</TableCell>
+      {transportRows.length ? (
+        <>
+          <p>Please confirm and submit the below entries.</p>
+          <Buttons payload={transportRows} />
+          <div style={{ margin: "20px" }}>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Colour</TableCell>
+                    <TableCell>Animal</TableCell>
+                    <TableCell>Transport</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell>AM/PM</TableCell>
                   </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-      <Buttons payload={transportRows} />
+                </TableHead>
+                <TableBody>
+                  {transportRows.map((row: transportData, index: number) => {
+                    const { zone, transport, date, am_pm } = row;
+                    return (
+                      <TableRow
+                        key={`${row.zone.animal}-${zone.colour}-${transport}-${date}-${am_pm}-${index}`}
+                      >
+                        <TableCell>{zone.colour}</TableCell>
+                        <TableCell>{zone.animal}</TableCell>
+                        <TableCell>{transport}</TableCell>
+                        <TableCell>{date}</TableCell>
+                        <TableCell>{am_pm}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+          <Buttons payload={transportRows} />
+        </>
+      ) : (
+        <>
+          <p>No results to submit.</p>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              window.location.reload(false);
+            }}
+          >
+            Refresh
+          </Button>
+        </>
+      )}
     </div>
   );
 }
